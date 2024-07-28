24f37839543e177ed75f7ebbc0813e6e1062480b
Hyeji <165968530+quokkavely@users.noreply.github.com>
2024-07-28 23:08:37 +0900

    Delete src/java/com/springboot directory

--- src/java/com/springboot/answer/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.answer.repository;
-
-import com.springboot.answer.entity.AnswerPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<AnswerPost, Long> {
-}
--- src/java/com/springboot/auth/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.auth.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginDto {
-    private String username;
-    private String password;
-}
